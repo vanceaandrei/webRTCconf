032ad35477d3dfcd33d32e5b9593de5e49eec19e
vanceaandrei <vancea.andrei94@yahoo.ro>
2016-03-08 00:28:57 +0200

    fixed showing webcam after login and redirect after login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,27 +6,6 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Webcam <br>
-                    <video autoplay id = "webcam" style = "transform: rotateY(180deg);
-    -webkit-transform:rotateY(180deg);"></video>
-                    <script type="text/javascript">
-                        var stream;
-                        var video = document.getElementById('webcam');
-                        navigator.webkitGetUserMedia(
-                            {video:true , audio: false}, // Options
-                            function(localMediaStream) { // Success
-                                stream = localMediaStream;
-                                video.src = window.webkitURL.createObjectURL(stream);
-                            },
-                            function(err) { // Failure
-                                alert('getUserMedia failed: Code ' + err.code);
-                            }
-                        );
-                         
-                    </script>
-                </div>
             </div>
         </div>
     </div>
